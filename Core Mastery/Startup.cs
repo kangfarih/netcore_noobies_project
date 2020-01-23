b59788caace03a3b8765ca1e3c08998c85d29db8
Farih Muhammad <farihul.ibad@gmail.com>
2020-01-23 22:43:53 +0700

    First step test

--- Core Mastery/Startup.cs
+++ Core Mastery/Startup.cs
@@ -27,7 +27,7 @@ namespace Core_Mastery
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!, Yeaaaah.");
             });
         }
     }
